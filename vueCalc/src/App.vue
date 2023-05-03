822b09ce8419183944779c6316050b740a781cc8
eviolspirid777 <eviolspirid@gmail.com>
2023-05-03 21:30:14 +0300

    change the css form for calculator and add equalty

--- vueCalc/src/App.vue
+++ vueCalc/src/App.vue
@@ -1,3 +1,37 @@
+<template>
+
+<div class="calculator">
+  <div class="calculator__output">{{textbox}}</div>
+  <div class="calculator__keys">
+    <button @click="plusClick">+</button>
+    <button @click="minusClick">-</button>
+    <button @click="multClick">*</button>
+    <button @click="divClick">/</button>
+    <button @click="delClick">del</button>
+    <button @click="nineClick">9</button>
+    <button @click="eightClick">8</button>
+    <button @click="sevenClick">7</button>
+    <button @click="sixClick">6</button>
+    <button @click="fiveClick">5</button>
+    <button @click="fourClick">4</button>
+    <button @click="threeClick">3</button>
+    <button @click="twoClick">2</button>
+    <button @click="oneClick">1</button>
+    <button @click="nulClick">0</button>
+    <button @click="equalClick">=</button>
+    <button @click="m1RClick">M1R</button>
+    <button @click="m1CClick">M1C</button>
+    <button @click="m1PlusClick">M1+</button>
+    <button @click="m1MinusClick">M1-</button>
+    <button @click="m2RClick">M2R</button>
+    <button @click="m2CClick">M2C</button>
+    <button @click="m2PlusClick">M2+</button>
+    <button @click="m2MinusClick">M2-</button>
+  </div>
+</div>
+
+</template>
+
 <script setup>
   import { ref } from 'vue'
   const textbox = ref('')
@@ -41,7 +75,7 @@
   }
   const equalClick = () =>{
     var result = 0;
-    var text = this.textbox.split(' ');
+    var text = textbox.value.split(' ');
     var oper1 = parseFloat(text[0]);
     var oper2 = parseFloat(text[2]);
     if(text[1] == '+')
@@ -53,6 +87,18 @@
     if(text[1] == '*')
         result = oper1 * oper2;
     textbox.value = result;
+  }
+  const plusClick = () =>{
+    textbox.value += ' + '
+  }
+  const minusClick = () =>{
+    textbox.value += ' - '
+  }
+  const divClick = () =>{
+    textbox.value += ' / '
+  }
+  const multClick = () =>{
+    textbox.value += ' * '
   }
   const m1RClick = () =>{
     textbox.value += M1.value
@@ -80,52 +126,8 @@
   }
 </script>
 
-<template>
-<div class="calculator-container">
-  <p>{{textbox}}</p>
-  <div id="app">
-    <h1>
-      <button @click="plusClick">+</button>
-      <button @click="minusClick">-</button>
-      <button @click="multClick">*</button>
-      <button @click="divClick">/</button>
-      <button @click="delClick">del</button>
-      <button @click="nineClick">9</button>
-      <button @click="eightClick">8</button>
-      <button @click="sevenClick">7</button>
-      <button @click="sixClick">6</button>
-      <button @click="fiveClick">5</button>
-      <button @click="fourClick">4</button>
-      <button @click="threeClick">3</button>
-      <button @click="twoClick">2</button>
-      <button @click="oneClick">1</button>
-      <button @click="nulClick">0</button>
-      <button @click="equalClick">=</button>
-      <button @click="m1RClick">M1R</button>
-      <button @click="m1CClick">M1C</button>
-      <button @click="m1PlusClick">M1+</button>
-      <button @click="m1MinusClick">M1-</button>
-      <button @click="m2RClick">M2R</button>
-      <button @click="m2CClick">M2C</button>
-      <button @click="m2PlusClick">M2+</button>
-      <button @click="m2MinusClick">M2-</button>
-    </h1>
-  </div>
-</div>
-</template>
 
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
