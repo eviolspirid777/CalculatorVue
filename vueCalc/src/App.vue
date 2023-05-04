ba2acaa02130523608e182f95517ff99d9fb4134
eviolspirid777 <eviolspirid@gmail.com>
2023-05-04 19:32:42 +0300

    добавил клавиши и предоставил обработку событий для них

--- vueCalc/src/App.vue
+++ vueCalc/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-
 <div class="calculator">
   <div class="calculator__output">{{textbox}}</div>
   <div class="calculator__keys">
@@ -7,29 +6,38 @@
     <button @click="minusClick">-</button>
     <button @click="multClick">*</button>
     <button @click="divClick">/</button>
-    <button @click="delClick">del</button>
     <button @click="nineClick">9</button>
     <button @click="eightClick">8</button>
     <button @click="sevenClick">7</button>
+    <button @click="delClick">del</button>
     <button @click="sixClick">6</button>
     <button @click="fiveClick">5</button>
     <button @click="fourClick">4</button>
+    <button @click="factClick">!n</button>
     <button @click="threeClick">3</button>
     <button @click="twoClick">2</button>
     <button @click="oneClick">1</button>
-    <button @click="nulClick">0</button>
     <button @click="equalClick">=</button>
-    <button @click="m1RClick">M1R</button>
-    <button @click="m1CClick">M1C</button>
+    <button @click="dotClick">,</button>
+    <button @click="nulClick">0</button>
+    <button @click="expClick">exp</button>
+    <button @click="exponentiationClick">x^y</button>
+    <button @click="cosClick">cos</button>
+    <button @click="sinClick">sin</button>
+    <button @click="tgClick">tg</button>
+    <button @click="arctgClick">arctg</button>
     <button @click="m1PlusClick">M1+</button>
     <button @click="m1MinusClick">M1-</button>
-    <button @click="m2RClick">M2R</button>
-    <button @click="m2CClick">M2C</button>
+    <button @click="m1RClick">M1R</button>
+    <button @click="m1CClick">M1C</button>
     <button @click="m2PlusClick">M2+</button>
     <button @click="m2MinusClick">M2-</button>
+    <button @click="m2RClick">M2R</button>
+    <button @click="m2CClick">M2C</button>
+    <button @click="lnClick">Ln</button>
+    <button @click="logClick">Log</button>
   </div>
 </div>
-
 </template>
 
 <script setup>
@@ -86,6 +94,8 @@
         result = oper1 / oper2;
     if(text[1] == '*')
         result = oper1 * oper2;
+    if(text[1] == '^')
+        result = Math.pow(oper1, oper2)
     textbox.value = result;
   }
   const plusClick = () =>{
@@ -107,10 +117,10 @@
     textbox.value += M2.value
   }
   const m1CClick = () =>{
-    M1 = 0
+    M1.value = 0
   }
   const m2CClick = () =>{
-    M2 = 0
+    M2.value = 0
   }
   const m1PlusClick = () =>{
     M1.value = parseFloat(M1.value) + parseFloat(textbox.value)
@@ -123,6 +133,38 @@
   }
   const m2MinusClick = () =>{
     M1.value = parseFloat(M2.value) - parseFloat(textbox.value)
+  }
+  const factClick = () => {
+    var result = 1
+    for(var i = 1; i <= parseFloat(textbox.value);i++){
+      result *= i
+    }
+    textbox.value = result
+  }
+  const dotClick = () =>{
+    textbox.value += `.`
+  }
+  const expClick = () =>{
+    textbox.value =  Math.exp(textbox.value)
+  }
+  const exponentiationClick = () => textbox.value += " ^ "
+  const cosClick = () => {
+    textbox.value = Math.cos(textbox.value);
+  }
+  const sinClick = () => {
+    textbox.value = Math.sin(textbox.value);
+  }
+  const tgClick = () => {
+    textbox.value = Math.tan(textbox.value);
+  }
+  const arctgClick = () => {
+    textbox.value = Math.atan(textbox.value);
+  }
+  const lnClick = () => {
+    textbox.value = Math.log(textbox.value)
+  }
+  const logClick = () => {
+                                                        //напиши обработку этого события
   }
 </script>
 
