48f2be57b280c94f248d89384ceb25d0679f7e74
eviolspirid777 <eviolspirid@gmail.com>
2023-05-15 18:58:19 +0300

    Изменил дизайн, добавил обработку наведения на клавишу HOVER, увеличил размер кал-ра, а также сделал css в одном файле

--- vueCalc/src/App.vue
+++ vueCalc/src/App.vue
@@ -2,43 +2,44 @@
   <div class="calculator">
     <div class="calculator__output">{{textbox}}</div>
     <div class="calculator__keys">
-      <button @click="plusClick">+</button>
-      <button @click="minusClick">-</button>
-      <button @click="multClick">*</button>
-      <button @click="divClick">/</button>
-      <button @click="nineClick">9</button>
-      <button @click="eightClick">8</button>
-      <button @click="sevenClick">7</button>
-      <button @click="delClick">&larr;</button>
-      <button @click="sixClick">6</button>
-      <button @click="fiveClick">5</button>
-      <button @click="fourClick">4</button>
-      <button @click="clearClick">C</button>
-      <button @click="threeClick">3</button>
-      <button @click="twoClick">2</button>
-      <button @click="oneClick">1</button>
-      <button class="calculator__key--enter" @click="equalClick">=</button>
-      <button @click="dotClick">,</button>
-      <button @click="nulClick">0</button>
-      <button @click="spaceClick">|__|</button>
-      <button @click="exponentiationClick">x^y</button>
-      <button @click="cosClick">cos</button>
-      <button @click="sinClick">sin</button>
-      <button @click="tgClick">tg</button>
-      <button @click="arctgClick">arctg</button>
-      <button @click="m1PlusClick">M1+</button>
-      <button @click="m1MinusClick">M1-</button>
-      <button @click="m1RClick">M1R</button>
-      <button @click="m1CClick">M1C</button>
-      <button @click="m2PlusClick">M2+</button>
-      <button @click="m2MinusClick">M2-</button>
-      <button @click="m2RClick">M2R</button>
-      <button @click="m2CClick">M2C</button>
-      <button @click="lnClick">Ln</button>
-      <button @click="logClick">Log</button>
-      <button @click="expClick">exp</button>
-      <button @click="factClick">!n</button>
-    </div>
+      <button @click="plusClick" class="calculator__key calculator__key--operator">+</button>
+      <button @click="minusClick" class="calculator__key calculator__key--operator">-</button>
+      <button @click="multClick" class="calculator__key calculator__key--operator">*</button>
+      <button @click="divClick" class="calculator__key calculator__key--operator">/</button>
+      <button @click="nineClick" class="calculator__key">9</button>
+      <button @click="eightClick" class="calculator__key">8</button>
+      <button @click="sevenClick" class="calculator__key">7</button>
+      <button @click="delClick" class="calculator__key calculator__key--operator">&larr;</button>
+      <button @click="sixClick" class="calculator__key">6</button>
+      <button @click="fiveClick" class="calculator__key">5</button>
+      <button @click="fourClick" class="calculator__key">4</button>
+      <button @click="clearClick" class="calculator__key calculator__key--operator">C</button>
+      <button @click="threeClick" class="calculator__key">3</button>
+      <button @click="twoClick" class="calculator__key">2</button>
+      <button @click="oneClick" class="calculator__key">1</button>
+      <button @click="equalClick" class="calculator__key calculator__key--enter">=</button>
+      <button @click="dotClick" class="calculator__key calculator__key--operator">,</button>
+      <button @click="nulClick" class="calculator__key">0</button>
+      <button @click="spaceClick" class="calculator__key calculator__key--operator">|__|</button>
+      <button @click="exponentiationClick" class="calculator__key calculator__key--operator">x^y</button>
+      <button @click="cosClick" class="calculator__key calculator__key--operator">cos</button>
+      <button @click="sinClick" class="calculator__key calculator__key--operator">sin</button>
+      <button @click="tgClick" class="calculator__key calculator__key--operator">tg</button>
+      <button @click="arctgClick" class="calculator__key calculator__key--operator">arctg</button>
+      <button @click="m1PlusClick" class="calculator__key calculator__key--memory">M1+</button>
+      <button @click="m1MinusClick" class="calculator__key calculator__key--memory">M1-</button>
+      <button @click="m1RClick" class="calculator__key calculator__key--memory">M1R</button>
+      <button @click="m1CClick" class="calculator__key calculator__key--memory">M1C</button>
+      <button @click="m2PlusClick" class="calculator__key calculator__key--memory">M2+</button>
+      <button @click="m2MinusClick" class="calculator__key calculator__key--memory">M2-</button>
+      <button @click="m2RClick" class="calculator__key calculator__key--memory">M2R</button>
+      <button @click="m2CClick" class="calculator__key calculator__key--memory">M2C</button>
+      <button @click="lnClick" class="calculator__key calculator__key--operator">Ln</button>
+      <button @click="logClick" class="calculator__key calculator__key--operator">Log</button>
+      <button @click="expClick" class="calculator__key calculator__key--operator">exp</button>
+      <button @click="factClick" class="calculator__key calculator__key--operator">!n</button>
+</div>
+
   </div>
 </template>
 <script>
@@ -136,58 +137,123 @@ export default {
       m1CClick(){
         this.M1 = 0
       },
-      m2CClic(){
+      m2CClick(){
         this.M2 = 0
       },
       m1PlusClick(){
         this.M1 = parseFloat(this.M1) + parseFloat(this.textbox)
       },
       m2PlusClick(){
-        this.M1 = parseFloat(this.M1) + parseFloat(this.textbox)
+        this.M2 = parseFloat(this.M1) + parseFloat(this.textbox)
       },
       m1MinusClick(){
         this.M1 = parseFloat(this.M1) - parseFloat(this.textbox)
       },
       m2MinusClick(){
-        this.M1 = parseFloat(this.M2) - parseFloat(this.textbox)
+        this.M2 = parseFloat(this.M2) - parseFloat(this.textbox)
       },
       factClick(){
         var result = 1
-        for(var i = 1; i <= parseFloat(this.textbox);i++){
-          result *= i
-        }
-        this.textbox = result
-      },
-      dotClick(){
-        this.textbox += `.`
-      },
-      spaceClick(){
-        this.textbox += ' '
-      },
-      expClick(){
-        this.textbox =  Math.exp(this.textbox)
-      },
-      exponentiationClick() {
-        this.textbox += " ^ "
-      },
-      cosClick () {
-        this.textbox = Math.cos(this.textbox);
-      },
-      sinClick () {
-        this.textbox = Math.sin(this.textbox);
-      },
-      tgClick () {
-        this.textbox = Math.tan(this.textbox);
-      },
-      arctgClick () {
-        this.textbox = Math.atan(this.textbox);
-      },
-      lnClick (){
-        this.textbox = Math.log(this.textbox);
-      },
-      logClick () {
-        this.textbox += "LOG "
-      },
-  }
+        for(var i = 1; i <=parseFloat(this.textbox);i++){
+result *= i
+}
+this.textbox = result
+},
+dotClick(){
+this.textbox += `.`
+},
+spaceClick(){
+this.textbox += ' '
+},
+expClick(){
+this.textbox = Math.exp(this.textbox)
+},
+exponentiationClick() {
+this.textbox += " ^ "
+},
+cosClick () {
+this.textbox = Math.cos(this.textbox);
+},
+sinClick () {
+this.textbox = Math.sin(this.textbox);
+},
+tgClick () {
+this.textbox = Math.tan(this.textbox);
+},
+arctgClick () {
+this.textbox = Math.atan(this.textbox);
+},
+lnClick (){
+this.textbox = Math.log(this.textbox);
+},
+logClick () {
+this.textbox += "LOG "
+},
+}
 }
 </script>
+
+<style>
+.calculator {
+  border-radius: 20px;
+  box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.15), 0px 2px 4px 0px rgba(0, 0, 0, 0.12);
+  margin-inline-start: auto;
+  margin-inline-end: auto;
+  margin-block-start: 32px;
+  max-inline-size: 1000px;
+  overflow: hidden;
+}
+.calculator__output {
+  height: 40px;
+  width: 989px;
+  background: hsl(203, 11%, 43%);
+  color: hsl(0, 0%, 100%);
+  font-size: 32px;
+  padding-block-start: 8px;
+  padding-block-end: 8px;
+  padding-inline-end: 12px;
+  text-align: end;
+}
+.calculator__keys {
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  grid-gap: 1px;
+  background: hsl(0, 4%, 5%);
+}
+.calculator__key {
+  background: hsla(207, 25%, 86%, 0.788);
+  border: none;
+  padding-block-start: 16px;
+  padding-block-end: 16px;
+  padding-inline-end: 192px;
+  padding-inline-start: 20px;
+  font-size: 24;
+}
+.calculator__key:hover {
+  background: hsla(207, 25%, 86%, 0.349);
+
+}
+.calculator__key:active,
+.calculator__key:focus {
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.3) inset;
+  outline: none;
+}
+.calculator__key--operator {
+  background: hsl(208, 25%, 86%);
+}
+.calculator__key--memory{
+  background: hsla(207, 25%, 86%, 0.623);
+}
+.calculator__key--operator:active {
+  background: hsl(208, 24%, 80%);
+}
+
+.calculator__key--enter{
+  background: hsla(207, 24%, 80%, 0.281);
+}
+
+.calculator__key--enter:hover {
+  background: hsla(0, 100%, 50%, 0.527);
+  cursor: pointer;
+}
+</style>
